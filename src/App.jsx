53c96dfd11a96e17b99854e9c9c1431106743874
JeikuDevvv <jakelourencevillar.dev@gmail.com>
2023-05-23 04:11:33 +0800

    done logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,93 @@
-import "./styles/app.css"
+import { useState } from "react";
+import "./styles/app.css";
 
 export default function App() {
-  return <>
-    <div className="main">
-      <div className="mainContainer">
-        <div className="formContainer">
-          <form className="newItemForm">
-            <div className="formContent">
-              <label htmlFor="item">New Task</label>
-              <input type="text" name="newItem" id="item" />
-              <button type="submit">Add Task</button>
-            </div>
-          </form>
+  const [newTask, setNewTask] = useState("");
+  const [task, setTask] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setTask((currentTask) => {
+      return [
+        ...currentTask,
+        { id: crypto.randomUUID(), title: newTask, completed: false },
+      ];
+    });
+
+    setNewTask("");
+  };
+
+  const toggleTask = (id, completed) => {
+    setTask((currentTask) => {
+      return currentTask.map((rTask) => {
+        if (rTask.id === id) {
+          return { ...rTask, completed };
+        }
+
+        return rTask;
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTask((currentTask) => {
+      return currentTask.filter((rTask) => rTask.id !== id);
+    });
+  };
+
+  return (
+    <>
+      <div className="main">
+        <div className="mainContainer">
+          <div className="formContainer">
+            <form onSubmit={handleSubmit} className="newItemForm">
+              <div className="formContent">
+                <label htmlFor="item">New Task</label>
+                <input
+                  value={newTask}
+                  onChange={(event) => setNewTask(event.target.value)}
+                  type="text"
+                  name="newItem"
+                  id="item"
+                />
+                <button type="submit">Add Task</button>
+              </div>
+            </form>
+          </div>
+          <div className="listContainer">
+            <h2>Task List</h2>
+            <hr />
+            <ul className="taskList">
+              {task.length === 0 && "No Task  ^.^ .!."}
+              {task.map((rTask) => {
+                return (
+                  <li key={rTask.id}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={rTask.completed}
+                        onChange={(event) =>
+                          toggleTask(rTask.id, event.target.checked)
+                        }
+                      />
+                      {rTask.title}
+                    </label>
+                    <button
+                      className="deleteTask"
+                      onClick={() => {
+                        deleteTask(rTask.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <div className="listContainer">
-        <h2>Task List</h2>
-        <ul className="taskList">
-          <li>
-            <label htmlFor="">
-              <input type="checkbox" />
-              Item 1
-            </label>
-          </li>
-        </ul>
       </div>
-      </div>   
-    </div>
-  </>
+    </>
+  );
 }
